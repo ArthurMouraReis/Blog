5a7b1956d5a7778a49eaee8f81e43317ccce7743
MacMini <macmini@MacMinis-Mac-mini.local>
2024-04-22 20:27:27 -0300

    Initial commit

---
+++ javascript/header.js
@@ -0,0 +1,9 @@
+
+
+function popularMenu(){
+    let menu = document.getElementById("header")
+    menu.innerHTML =  '<h1>Blog de Espanhol</h1> <nav id="menu"><a href="/">Início</a> <a href="/escola">Escola</a> <a href="/artigos">Artigos</a> <a href="/sobre">Sobre</a> <a href="/contato">Contato</a></nav>'
+}
+
+
+popularMenu()
